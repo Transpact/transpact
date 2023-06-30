57d50bd2bfca7d11c063d4d8b7002b574567314c
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-07-01 03:13:12 +0530

    Fonts Updated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -71,6 +71,10 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        heading: ["CalSans", "sans-serif"],
+        sans: ["Inter", "sans-serif"],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
