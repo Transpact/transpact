986e0ae3ae9a3bf6884f20b3f31ac7a8b874e812
AtmegaBuzz <swapnilshinde9382@gmail.com>
2023-07-01 01:56:48 +0530

    added user creation validation checks

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,10 +1,27 @@
 use std::fmt::Debug;
+use sha2::digest::crypto_common::InnerUser;
+use sha2::{Digest,Sha256};
+extern crate hex;
 
 // Find all our documentation at https://docs.near.org
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedMap;
 use near_sdk::{log, near_bindgen, AccountId, PublicKey, env};
 use near_sdk::serde::{Deserialize,Serialize};
+
+// function to generate User hash
+fn generate_user_hash(name: &str, email: &str) -> String{
+
+    let mut hasher = Sha256::new();
+
+    hasher.update(format!("name: {}, email: {}",name,email));
+
+    let hash_result = hasher.finalize();
+    let hash_hex_string = hex::encode(hash_result);
+
+    return hash_hex_string
+
+}
 
 #[derive(Clone,PartialEq,BorshDeserialize,BorshSerialize,Serialize,Deserialize,Debug)]
 #[serde(tag = "enum", crate = "near_sdk::serde")]
@@ -28,10 +45,11 @@ pub struct Lister{
 impl Lister {
     fn new(name: String, email: String, account_id:AccountId) -> Self {
 
+        let user_hash = generate_user_hash(&name,&email);
         Self {
             name,
             email,
-            user_hash: "dasjd".to_string(),
+            user_hash: user_hash,
             account_id: account_id,
             account_status: AccountStatus::UNVERIFIED,
         }
@@ -53,10 +71,11 @@ pub struct Contractor{
 impl Contractor {
     fn new(name: String, email: String, account_id:AccountId) -> Self {
 
+        let user_hash = generate_user_hash(&name,&email);
         Self {
             name,
             email,
-            user_hash: "dasjd".to_string(),
+            user_hash: user_hash,
             account_id: account_id,
             account_status: AccountStatus::UNVERIFIED,
         }
@@ -96,6 +115,9 @@ impl Contract {
         let listers_account_id = env::signer_account_id();
         let listers_pub_key = env::signer_account_pk();
 
+        let userExists = self.contractors.get(&listers_pub_key);
+        assert_ne!(userExists,None,"User already Exists");
+
         let mut lister = Lister::new(name,email,listers_account_id);
         lister.account_status = AccountStatus::ACTIVE;
 
@@ -106,6 +128,31 @@ impl Contract {
         return Response{
             status: String::from("CREATED"),
             message: String::from("Lister is created successfully"),
+            data: None
+        };
+    }
+
+    pub fn create_contractor(&mut self, name: String, email: String) -> Response{
+
+        
+        let contractors_account_id = env::signer_account_id();
+        let contractors_pub_key = env::signer_account_pk();
+        
+        let userExists = self.contractors.get(&contractors_pub_key);
+        assert_ne!(userExists,None,"User already Exists");
+       
+        // assert_eq!(userExists,),"User already exists");
+
+        let mut contractor = Contractor::new(name,email,contractors_account_id);
+        contractor.account_status = AccountStatus::ACTIVE;
+
+        self.contractors.insert(&contractors_pub_key,&contractor);
+
+
+        log!("Contractor is created successfully name: {}, Total Contractors: {}", contractor.name, self.contractors.len());
+        return Response{
+            status: String::from("CREATED"),
+            message: String::from("Contractor is created successfully"),
             data: None
         };
     }
