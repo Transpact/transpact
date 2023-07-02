c062fc69779f9218d3a064e3d0d061f083bf310d
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-07-02 18:20:08 +0530

    Minor Changes

--- frontend/pages/contract-detail.tsx
+++ frontend/pages/contract-detail.tsx
@@ -12,6 +12,7 @@ import { Link } from "react-router-dom";
 
 import { useParams } from "react-router-dom";
 import { SAMPLE_CONTRACT } from "@/lib/data";
+import { formatDate } from "@/lib/utils";
 
 interface ContractDetailsPageProps {}
 
@@ -20,9 +21,13 @@ const ContractDetailsPage: React.FC<ContractDetailsPageProps> = ({}) => {
   const { id: contractId } = useParams();
 
   const [loading, setLoading] = useState(false);
-  const [contract, setContract] = useState<Contract | null>(null);
+  const [contract, setContract] = useState<Contract | null | undefined>(
+    undefined
+  );
 
   const getContracts = async () => {
+    console.log(contractId);
+
     if (loading || !contractId) return;
 
     setLoading(true);
@@ -31,8 +36,11 @@ const ContractDetailsPage: React.FC<ContractDetailsPageProps> = ({}) => {
       // TODO: Get from blockchain
       const contract: Contract = SAMPLE_CONTRACT[0];
 
+      console.log(contract);
+
       setContract(contract);
     } catch (e) {
+      setContract(null);
       console.log(e);
     } finally {
       setLoading(false);
@@ -41,21 +49,38 @@ const ContractDetailsPage: React.FC<ContractDetailsPageProps> = ({}) => {
 
   useEffect(() => {
     getContracts();
-  }, []);
+  }, [contractId]);
+
+  if (contract === null) {
+    return <p>Loading</p>;
+  }
 
   return (
     <DashboardLayout
       type="none"
-      loading={loading}
+      loading={loading || contract === undefined}
       heading="Contract Info"
       text="View and bid for contract"
       buttonLabel=""
     >
       <DashboardShell>
-        <DashboardHeader
-          heading={contract?.name ?? ""}
-          text={contract?.description ?? ""}
-        ></DashboardHeader>
+        <DashboardHeader heading={contract?.name ?? ""} text={""} />
+
+        <p>
+          <span className="font-bold">START: </span>{" "}
+          {formatDate(contract?.startDate ?? new Date())}
+        </p>
+
+        <p>
+          <span className="font-bold">DEADLINE: </span>{" "}
+          {formatDate(contract?.endDate ?? new Date())}
+        </p>
+
+        <p>
+          <span className="font-bold">QUATATION: </span> ${contract?.amount}
+        </p>
+
+        <p className="mt-6">{contract?.description}</p>
       </DashboardShell>
     </DashboardLayout>
   );
