1602252aba2bef93104ce72f34c3a8758adb3eda
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-07-02 19:27:27 +0530

    Added URL

--- frontend/config/site.ts
+++ frontend/config/site.ts
@@ -3,10 +3,10 @@ import { SiteConfig } from "types";
 export const siteConfig: SiteConfig = {
   name: "Transpact",
   description: "An DApp based on NEAR Protocol",
-  url: "https://tx.shadcn.com",
+  url: "https://main--luxury-entremet-53224c.netlify.app/",
   ogImage: "https://tx.shadcn.com/og.jpg",
   links: {
-    twitter: "https://twitter.com/",
+    twitter: "https://devpost.com/software/transparent-contract",
     github: "https://github.com/hackArchive/Transpact/",
   },
 };
