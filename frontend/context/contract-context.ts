309239b37685865a9948c534fedeb1f1824b0c66
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-07-25 19:30:01 +0530

    React Frontend Removed

--- frontend/context/contract-context.ts
+++
@@ -1,9 +0,0 @@
-import { Wallet } from "@/near-wallet";
-import { createContext } from "react";
-
-export type ContractContextType = {
-  contracts: Contract[];
-  setContracts: (val: Contract[]) => void;
-};
-
-export const ContractContext = createContext<ContractContextType | null>(null);
