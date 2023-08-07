95d8341c84f3a6f3fb839fca46ba0dd7f8c42dd0
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-08-07 17:15:07 +0530

    Revert "Landing page"
    This reverts commit 5929f6e79af3c70f9ff0c11a4a10f4d5ed6287a9.

--- prisma/migrations/20230802180017_user_socials_experience/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "experience" INTEGER DEFAULT 0,
-ADD COLUMN     "website" TEXT DEFAULT '';
--- src/components/types.ts
+++
@@ -1,9 +0,0 @@
-export interface Contract {
-  id: number;
-  title: string;
-  description: string;
-  startDate: Date;
-  endDate: Date; 
-  amount: number;
-  status: "Listed" | "Pending" | "Completed";
-}
