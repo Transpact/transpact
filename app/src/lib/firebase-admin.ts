76ef0c529c3c786500d0148f79d3dc8ff91f3f80
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-09-04 19:01:59 +0530

    Added Firebase Storage

---
+++ app/src/lib/firebase-admin.ts
@@ -0,0 +1,19 @@
+import admin, { getApps, initializeApp, cert } from "firebase-admin/app"
+import { getStorage } from "firebase-admin/storage"
+import serviceAccount from "../../credentials/firebaseServiceAccount.json"
+
+if (!getApps().length) {
+  try {
+    initializeApp({
+      // @ts-ignore
+      credential: cert(serviceAccount),
+      storageBucket: "gs://transpact-e8548.appspot.com",
+    })
+  } catch (error: any) {
+    console.log("Firebase admin initialization error", error.stack)
+  }
+}
+
+const bucket = getStorage().bucket()
+
+export { bucket }
